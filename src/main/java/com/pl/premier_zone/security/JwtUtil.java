3fe4188d0e64e55c7d83025888a718fc6793f40b
Marvin <marvindillenseger@gmail.com>
2025-03-23 22:11:58 +0100

    Add JwtUtil class for JWT token generation and claim extraction

---
+++ src/main/java/com/pl/premier_zone/security/JwtUtil.java
@@ -0,0 +1,32 @@
+package com.pl.premier_zone.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+    private static final String SECRET_KEY = "secret";
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 heures
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+        return claimsResolver.apply(claims);
+    }
+}
